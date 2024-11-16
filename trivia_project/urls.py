bf14cc61ba2fb7ef89353b67cab684326d5fbc8e
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2024-11-16 18:19:27 +0300

    Add namespace to trivia_app URLs for improved URL management

--- trivia_project/urls.py
+++ trivia_project/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('trivia_app.urls'))
+    path('', include('trivia_app.urls', namespace='trivia_app')),
 ]
