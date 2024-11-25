eeb28be30e11f9f8b4a2e467be7e3d570d911db5
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2024-11-26 00:23:46 +0300

    Update trivia app URL patterns to include 'trivia/' prefix for improved routing

--- trivia_app/urls.py
+++ trivia_app/urls.py
@@ -6,8 +6,8 @@ app_name = 'trivia_app'
 
 urlpatterns = [
     path('trivia/', views.home, name='home'),
-    path('categories/', views.categories, name='categories'),
-    path('category/<int:category_id>/<int:question_index>/', views.question_page, name='question_page'),
-    path('category/<int:category_id>/results/', views.results, name='results'),
-    path('category/<int:category_id>/<int:question_index>/save/', views.save_answer, name='save_answer'),
+    path('trivia/categories/', views.categories, name='categories'),
+    path('trivia/category/<int:category_id>/<int:question_index>/', views.question_page, name='question_page'),
+    path('trivia/category/<int:category_id>/results/', views.results, name='results'),
+    path('trivia/category/<int:category_id>/<int:question_index>/save/', views.save_answer, name='save_answer'),
 ]
