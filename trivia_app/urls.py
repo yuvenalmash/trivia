511aafc25e30747fe6ce423a4c1d469cfbb4bd1d
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2024-11-25 22:49:21 +0300

    Update URL path for home view in trivia app

--- trivia_app/urls.py
+++ trivia_app/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'trivia_app'
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('trivia/', views.home, name='home'),
     path('categories/', views.categories, name='categories'),
     path('category/<int:category_id>/<int:question_index>/', views.question_page, name='question_page'),
     path('category/<int:category_id>/results/', views.results, name='results'),
